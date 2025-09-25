6af495e35b6babb27329aacf2344736f5bfadade
Jorge Muñoz Molina <172324959+joralmumo@users.noreply.github.com>
2025-09-25 18:22:18 -0300

    Fully-restored
    restaurado al 100%

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { CotizadorComponent } from './pages/cotizador/cotizador.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
 
 export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'acerca-de', component: AcercaDeComponent},
     {path: 'cotizador', component: CotizadorComponent},
     {path: '', component: HomeComponent},
     {path: '**', redirectTo: 'home'} // en caso de 404
