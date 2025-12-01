9c27d70b83f0d2a8f8c7e0b4f2f6db10f77d1baf
valjamettduoc <val.jamett@duocuc.cl>
2025-12-01 02:03:12 -0300

    separadores de decimales update
    Se cambiaron "," por "."

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
 import { routes } from './app.routes';
 
+// Registrar locale español
+registerLocaleData(localeEs, 'es');
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'es' }
+  ]
 };
