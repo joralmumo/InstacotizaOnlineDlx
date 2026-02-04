a8389bf975c38aa403ea5fd4943079f688c0ce5e
Jorge Alberto Muñoz Molina <joralmumo@hotmail.com>
2026-02-04 07:41:53 -0300

    electron.

---
+++ electron/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const startUrl = process.env.ELECTRON_START_URL || `file://${path.join(__dirname, '..', 'dist', 'instacotiza-requete-online', 'index.html')}`;
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+  win.loadURL(startUrl);
+  if (process.env.ELECTRON_START_URL) {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
